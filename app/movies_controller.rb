adf04079450235d633730f7fe76923c311435166
Felice Forby <feliceforby@gmail.com>
2018-06-25 22:43:09 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = { })
+def can_be_created_in_a_block(args = {})
   Movie.create do |m|
     args.each do |attribute, value|
       m.send("#{attribute}=", value)
