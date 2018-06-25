e23dd3b5624b1c0c091c6aef340dca53788ee58f
Felice Forby <feliceforby@gmail.com>
2018-06-25 22:24:30 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_with_a_hash_of_attributes
   # Movie.new(attributes)
 end
 
-def can_be_created_in_a_block(args = {title: "Kill Bill", lead: "Uma Thurman"})
+def can_be_created_in_a_block(args = {title: "Home Alone", lead: "Uma Thurman"})
   Movie.create do |m|
     m.title = args[:title]
     m.lead = args[:lead]
